7e565d7d4ac960e65000f7c9b11d92c8d5769a37
nothengineer <kiwami.group.yuya@gmail.com>
2021-02-20 15:57:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'marubatugame'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:nothengineer/https://github.com/nothengineer/marubatugame.git'
+set :repo_url,  'git@github.com:nothengineer/marubatugame.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
